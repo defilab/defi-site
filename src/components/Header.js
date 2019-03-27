c1730968f0ceba9f24c5100a59770273e919d8fd
kris12138 <542704841@qq.com>
2019-03-27 13:50:34 +0800

    ds-2

--- src/components/Header.js
+++ src/components/Header.js
@@ -22,67 +22,47 @@ class Header extends Component {
     const changeCountry = ({ key }) => {
       setLocle(key)
       store.dispatch({type: key})
-      for(let  i in languageLabels){
-        if(i===key){
-          let obj = document.getElementById(key);
-          obj.style.backgroundColor='#e6f7ff';
-          obj.style.color='#1890ff';
-        }
-        else{
-          let obj = document.getElementById(i)
-          obj.style.color='rgba(0, 0, 0, 0.65)';
-          obj.style.backgroundColor='#fff';
-        }
-
-      }
     };
     const menu = (
-      <Menu onClick={changeCountry}  defaultSelectedKeys={'en-PH'}>
-        <Menu.Item key="en-PH" id="en-PH" >Philippines</Menu.Item>
-        <Menu.Item key="id-ID" id="id-ID">Indonesian</Menu.Item>
+      <Menu className='submenu-active' onClick={changeCountry} selectable defaultSelectedKeys={'en-PH'}>
+        <Menu.Item key="en-PH" id="en-PH"  >Philippines</Menu.Item>
+        <Menu.Item key="id-ID" id="id-ID" >Indonesian</Menu.Item>
         <Menu.Item key="vi-VN" id="vi-VN">Vietnamese</Menu.Item>
       </Menu>
     );
     return (
-        <Row className="header" type="flex" align="middle" justify="center" style={{
-          position: transparent ? 'absolute' : 'static',
-          backgroundColor: transparent ? 'rgba(24, 40, 72, 0.2)' : '#182848'
-        }}>
-          <Row className="content" type="flex" align="middle">
-            <Col span={14}>
-              <Row type="flex" align="middle">
-                <div><Link to="/"><img className="logo" src={logo} alt="logo" /></Link></div>
+      <Row className="header" type="flex" align="middle" justify="center" style={{
+        position: transparent ? 'absolute' : 'static', backgroundColor: transparent ? 'rgba(24, 40, 72, 0.2)' : '#182848'}}>
+        <Row className="content" type="flex" align="middle">
+          <Col span={14}>
+            <Row type="flex" align="middle">
+              <div><Link to="/"><img className="logo" src={logo} alt="logo" /></Link></div>
                 <div>
                   <Menu className="nav" theme="dark" mode="horizontal" selectedKeys={[activeLink]}>
-                    <SubMenu className={activeLink === 'products' ? 'submenu-active' : ''} key="products"
-                             title="Products">
+                    <SubMenu className={activeLink === 'products' ? 'submenu-active' : ''} key="products" title="Products">
                       <Menu.Item><Link to="/products">Credit Fraud Detection</Link></Menu.Item>
                     </SubMenu>
                     <Menu.Item key="pricing"><Link to="/pricing">Pricing</Link></Menu.Item>
                     <Menu.Item key="company"><Link to="/company">About</Link></Menu.Item>
                   </Menu>
                 </div>
-              </Row>
-            </Col>
-            <Col span={10}>
-              <Row type="flex" justify="end">
+            </Row>
+          </Col>
+          <Col span={10}>
+            <Row type="flex" justify="end">
                 <a className="header-link" href="/white-paper.pdf">White Paper</a>
                 <div style={{ width: '30px' }} />
-                <a className="header-link" href="https://portal.test.defilab.com">
-
-                <Dropdown overlay={menu}>
-                <a className="ant-dropdown-link" href="#">
-                Language 
-                </a>
-                </Dropdown>
+                <a className=" header-link" href="https://portal.test.defilab.com" >     
+                  <Dropdown overlay={menu} >
+                    <a className="ant-dropdown-link " href="#">Language</a>
+                  </Dropdown>
                 </a>
                 <div style={{ width: '30px' }} />
                 <a className="header-link" href="https://portal.test.defilab.com">Sign In</a>
-
               </Row>
-            </Col>
-          </Row>
+          </Col>
         </Row>
+      </Row>
     );
   }
 }
