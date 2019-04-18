e2ecfd77844e1221b986f946e85bdc2df7db9594
Dong Han <dong.han@points.org>
2019-04-18 14:05:19 +0800

    add more partners

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -36,48 +36,56 @@ import partner4 from '../assets/partner_4.png'
 import partner5 from '../assets/partner_5.png'
 import partner6 from '../assets/partner_6.png'
 import partner7 from '../assets/partner_7.png'
+import partner8 from '../assets/partner_8.png'
+import partner9 from '../assets/partner_9.png'
+import partner10 from '../assets/partner_10.png'
+import partner11 from '../assets/partner_11.png'
+import partner12 from '../assets/partner_12.png'
+import partner13 from '../assets/partner_13.png'
 
 const VisionTile = (props) => {
   const { title, logo, background } = props;
 
   return (
-      <Row className={[styles.visionTile, globalStyles.backgroundImage].join(' ')} type="flex" align="middle"
-           style={{ backgroundImage: `url(${background})` }}>
-        <Col span={16}>
-          <div className={styles.visionTitle}>{title}</div>
-        </Col>
-        <Col span={8}>
-          <img src={logo} alt={title} style={{ width: '100px', height: '100px' }} />
-        </Col>
-      </Row>
+    <Row className={[styles.visionTile, globalStyles.backgroundImage].join(' ')}
+         type="flex"
+         align="middle"
+         style={{ backgroundImage: `url(${background})` }}>
+      <Col span={16}>
+        <div className={styles.visionTitle}>{title}</div>
+      </Col>
+      <Col span={8}>
+        <img src={logo} alt={title} style={{ width: '100px', height: '100px' }} />
+      </Col>
+    </Row>
   );
 };
 
 const Vision = () => (
-    <Row className={globalStyles.section} type="flex" justify="center">
-      <div className={globalStyles.content}>
-        <div className={globalStyles.sectionTitle}>Our Vision</div>
-        <div style={{ fontSize: '26px', padding: '0 8px' }}>Defi aims to define state-of-art inclusive finance with:
-        </div>
-        <Spacing />
-        <Row>
-          <Col span={12}>
-            <VisionTile title="Access to credit" logo={visionLogo1} background={visionBg1} />
-          </Col>
-          <Col span={12}>
-            <VisionTile title="Data portability" logo={visionLogo2} background={visionBg2} />
-          </Col>
-        </Row>
-        <Row>
-          <Col span={12}>
-            <VisionTile title="Identity proofing" logo={visionLogo3} background={visionBg3} />
-          </Col>
-          <Col span={12}>
-            <VisionTile title="Privacy-preserving computation" logo={visionLogo4} background={visionBg4} />
-          </Col>
-        </Row>
+  <Row className={globalStyles.section} type="flex" justify="center">
+    <div className={globalStyles.content}>
+      <div className={globalStyles.sectionTitle}>Our Vision</div>
+      <div style={{ fontSize: '26px', padding: '0 8px' }}>Defi aims to define state-of-art inclusive finance with:
       </div>
-    </Row>
+      <Spacing />
+      <Row>
+        <Col span={12}>
+          <VisionTile title="Access to credit" logo={visionLogo1} background={visionBg1} />
+        </Col>
+        <Col span={12}>
+          <VisionTile title="Data portability" logo={visionLogo2} background={visionBg2} />
+        </Col>
+      </Row>
+      <Row>
+        <Col span={12}>
+          <VisionTile title="Identity proofing" logo={visionLogo3} background={visionBg3} />
+        </Col>
+        <Col span={12}>
+          <VisionTile title="Privacy-preserving computation" logo={visionLogo4} background={visionBg4} />
+        </Col>
+      </Row>
+    </div>
+  </Row>
 )
 
 class ProductTile extends Component {
@@ -86,150 +94,177 @@ class ProductTile extends Component {
   }
 
   render = () => (
-      <div className={globalStyles.backgroundImage} style={{ backgroundImage: `url(${this.props.background})` }}>
-        <Row className={styles.productTile} type="flex" align="bottom"
-             onMouseEnter={() => this.setState({ active: true })} onMouseLeave={() => this.setState({ active: false })}>
-          <Col>
-            <div><img src={this.props.icon} alt="" style={{ width: '34px', height: '30px' }} /></div>
-            <div style={{ fontSize: '22px' }}>{this.props.title}</div>
-            <div style={{ height: '60px' }}>{this.props.desc}</div>
-            {this.state.active ?
-                <Button htmlType="submit" style={{ color: '#182848', marginTop: '20px' }}>Details</Button> : null}
-          </Col>
-        </Row>
-      </div>
+    <div className={globalStyles.backgroundImage} style={{ backgroundImage: `url(${this.props.background})` }}>
+      <Row className={styles.productTile}
+           type="flex"
+           align="bottom"
+           onMouseEnter={() => this.setState({ active: true })}
+           onMouseLeave={() => this.setState({ active: false })}>
+        <Col>
+          <div><img src={this.props.icon} alt="" style={{ width: '34px', height: '30px' }} /></div>
+          <div style={{ fontSize: '22px' }}>{this.props.title}</div>
+          <div style={{ height: '60px' }}>{this.props.desc}</div>
+          {this.state.active ?
+            <Button htmlType="submit" style={{ color: '#182848', marginTop: '20px' }}>Details</Button> : null}
+        </Col>
+      </Row>
+    </div>
   )
 }
 
 const Solutions = () => (
-    <Row className={[globalStyles.section, styles.solutions].join(' ')} type="flex" justify="center"
-         style={{ backgroundImage: `url(${solutionsBg})` }}>
-      <div className={globalStyles.content}>
-        <div className={globalStyles.sectionTitle}>Our Solutions</div>
-        <Row type="flex" gutter={10}>
-          <Col className={styles.solutionTile} span={10}>
-            <div className={styles.solutionHeader}>
-              <div className={styles.solutionTitle}>
-                Enterprise Data Collaboration
-              </div>
-              <div className={styles.solutionDesc}>
-                Financial institutions can contribute data and models to better identify, evaluate and control risk. As
-                a result, more individuals will have access to better financial services.
-              </div>
+  <Row className={[globalStyles.section, styles.solutions].join(' ')}
+       type="flex"
+       justify="center"
+       style={{ backgroundImage: `url(${solutionsBg})` }}>
+    <div className={globalStyles.content}>
+      <div className={globalStyles.sectionTitle}>Our Solutions</div>
+      <Row type="flex" gutter={10}>
+        <Col className={styles.solutionTile} span={10}>
+          <div className={styles.solutionHeader}>
+            <div className={styles.solutionTitle}>
+              Enterprise Data Collaboration
             </div>
-            <div className={styles.solutionBody}>
-              <ProductTile background={productsBg1} icon={productIcon1} title="Credit Fraud Detection Solution"
-                           desc={`Secure joint computation platform for lending companies to detect credit fraud.`} />
+            <div className={styles.solutionDesc}>
+              Financial institutions can contribute data and models to better identify, evaluate and control risk. As
+              a result, more individuals will have access to better financial services.
             </div>
-          </Col>
-          <Col className={styles.solutionTile} span={14}>
-            <div className={styles.solutionHeader}>
-              <div className={styles.solutionTitle}>
-                Personal Data Wallet
-              </div>
-              <div className={styles.solutionDesc}>
-                Defi makes sure your data is protected and owned by you. With appropriate management and authorization,
-                your data could help you get better services and even token rewards.
-              </div>
+          </div>
+          <div className={styles.solutionBody}>
+            <ProductTile background={productsBg1}
+                         icon={productIcon1}
+                         title="Credit Fraud Detection Solution"
+                         desc={`Secure joint computation platform for lending companies to detect credit fraud.`} />
+          </div>
+        </Col>
+        <Col className={styles.solutionTile} span={14}>
+          <div className={styles.solutionHeader}>
+            <div className={styles.solutionTitle}>
+              Personal Data Wallet
             </div>
-            <div className={styles.solutionBody}>
-              <Row tyle="flex" gutter={2}>
-                <Col span={12}>
-                  <ProductTile background={productsBg2} icon={productIcon2} title="Coinsta"
-                               desc="News. Personal Data. Cryptocurrencies. Manage all your digital assets in one app." />
-                </Col>
-                <Col span={12}>
-                  <ProductTile background={productsBg3} icon={productIcon3} title="CreditMe"
-                               desc="One stop shop to get the loan best suited for you with your own data" />
-                </Col>
-              </Row>
+            <div className={styles.solutionDesc}>
+              Defi makes sure your data is protected and owned by you. With appropriate management and authorization,
+              your data could help you get better services and even token rewards.
             </div>
-          </Col>
-        </Row>
-      </div>
-    </Row>
+          </div>
+          <div className={styles.solutionBody}>
+            <Row tyle="flex" gutter={2}>
+              <Col span={12}>
+                <ProductTile background={productsBg2}
+                             icon={productIcon2}
+                             title="Coinsta"
+                             desc="News. Personal Data. Cryptocurrencies. Manage all your digital assets in one app." />
+              </Col>
+              <Col span={12}>
+                <ProductTile background={productsBg3}
+                             icon={productIcon3}
+                             title="CreditMe"
+                             desc="One stop shop to get the loan best suited for you with your own data" />
+              </Col>
+            </Row>
+          </div>
+        </Col>
+      </Row>
+    </div>
+  </Row>
 )
 
 const TechnologyTile = (props) => (
-    <Row className={styles.technologyTile} type="flex" justify="center">
-      <Col>
-        <div><img src={props.icon} alt={props.title} style={{ width: '80px', height: '80px' }} /></div>
-        <Spacing height="15px" />
-        <div>{props.title}</div>
-      </Col>
-    </Row>
+  <Row className={styles.technologyTile} type="flex" justify="center">
+    <Col>
+      <div><img src={props.icon} alt={props.title} style={{ width: '80px', height: '80px' }} /></div>
+      <Spacing height="15px" />
+      <div>{props.title}</div>
+    </Col>
+  </Row>
 )
 
 const Technologies = () => (
-    <Row className={globalStyles.section} type="flex" justify="center">
-      <div className={globalStyles.content}>
-        <div className={globalStyles.sectionTitle}>Core Technologies</div>
-        <div style={{ textAlign: 'center' }}>By using technologies including blockchain, secure multi-party computation
-          and machine learning, Defi can standardize, encapsulate and encrypt data to enable multi-party data
-          collaboration.
-        </div>
-        <Spacing height="50px" />
-        <Row>
-          <Col span={8}><TechnologyTile title="Distributed Ledger" icon={techIcon1} /></Col>
-          <Col span={8}><TechnologyTile title="Smart Contract" icon={techIcon2} /></Col>
-          <Col span={8}><TechnologyTile title="Trusted Execution Environment" icon={techIcon3} /></Col>
-          <Col span={8}><TechnologyTile title="Data Encryption & Obfuscation" icon={techIcon4} /></Col>
-          <Col span={8}><TechnologyTile title="Zero-knowledge Proof" icon={techIcon5} /></Col>
-          <Col span={8}><TechnologyTile title="Data Indexing & Search" icon={techIcon6} /></Col>
-          <Col span={8}><TechnologyTile title="Asymmetric  Cryptography" icon={techIcon7} /></Col>
-          <Col span={8}><TechnologyTile title="Differential Privacy" icon={techIcon8} /></Col>
-          <Col span={8}><TechnologyTile title="Federated Computation" icon={techIcon9} /></Col>
-        </Row>
+  <Row className={globalStyles.section} type="flex" justify="center">
+    <div className={globalStyles.content}>
+      <div className={globalStyles.sectionTitle}>Core Technologies</div>
+      <div style={{ textAlign: 'center' }}>By using technologies including blockchain, secure multi-party computation
+        and machine learning, Defi can standardize, encapsulate and encrypt data to enable multi-party data
+        collaboration.
       </div>
-    </Row>
+      <Spacing height="50px" />
+      <Row>
+        <Col span={8}><TechnologyTile title="Distributed Ledger" icon={techIcon1} /></Col>
+        <Col span={8}><TechnologyTile title="Smart Contract" icon={techIcon2} /></Col>
+        <Col span={8}><TechnologyTile title="Trusted Execution Environment" icon={techIcon3} /></Col>
+        <Col span={8}><TechnologyTile title="Data Encryption & Obfuscation" icon={techIcon4} /></Col>
+        <Col span={8}><TechnologyTile title="Zero-knowledge Proof" icon={techIcon5} /></Col>
+        <Col span={8}><TechnologyTile title="Data Indexing & Search" icon={techIcon6} /></Col>
+        <Col span={8}><TechnologyTile title="Asymmetric  Cryptography" icon={techIcon7} /></Col>
+        <Col span={8}><TechnologyTile title="Differential Privacy" icon={techIcon8} /></Col>
+        <Col span={8}><TechnologyTile title="Federated Computation" icon={techIcon9} /></Col>
+      </Row>
+    </div>
+  </Row>
 )
 
+const Partner = ({ logo }) => (
+  <Col span={3.5}>
+    <img src={logo} alt="" style={{ width: '111px', height: '52px' }} />
+  </Col>
+);
+
 const Partners = () => (
-    <Row className={[globalStyles.section, styles.partners].join(' ')} type="flex" justify="center"
-         style={{ backgroundImage: `url(${partnersBg})` }}>
-      <div className={globalStyles.content}>
-        <div className={globalStyles.sectionTitle}>Strategic Partners & Investors</div>
-        <Row type="flex" justify="center" gutter={40}>
-          <Col span={5}><img src={partner1} alt=""
-                             style={{ width: '159px', height: '75px', marginBottom: '30px' }} /></Col>
-          <Col span={5}><img src={partner2} alt="" style={{ width: '159px', height: '75px' }} /></Col>
-          <Col span={5}><img src={partner3} alt="" style={{ width: '159px', height: '75px' }} /></Col>
-          <Col span={5}><img src={partner4} alt="" style={{ width: '159px', height: '75px' }} /></Col>
-          <Col span={5}><img src={partner5} alt="" style={{ width: '159px', height: '75px' }} /></Col>
-          <Col span={5}><img src={partner6} alt="" style={{ width: '159px', height: '75px' }} /></Col>
-          <Col span={5}><img src={partner7} alt="" style={{ width: '159px', height: '75px' }} /></Col>
-        </Row>
-      </div>
-    </Row>
+  <Row className={[globalStyles.section, styles.partners].join(' ')}
+       type="flex"
+       justify="center"
+       style={{ backgroundImage: `url(${partnersBg})` }}>
+    <div className={globalStyles.content}>
+      <div className={globalStyles.sectionTitle}>Strategic Partners & Investors</div>
+      <Row type="flex" justify="center" gutter={30}>
+        <Partner logo={partner1} />
+        <Partner logo={partner2} />
+        <Partner logo={partner3} />
+        <Partner logo={partner4} />
+        <Partner logo={partner5} />
+        <Partner logo={partner6} />
+      </Row>
+      <div style={{ height: '26px' }} />
+      <Row type="flex" justify="center" gutter={30}>
+        <Partner logo={partner7} />
+        <Partner logo={partner8} />
+        <Partner logo={partner9} />
+        <Partner logo={partner10} />
+        <Partner logo={partner11} />
+        <Partner logo={partner12} />
+        <Partner logo={partner13} />
+      </Row>
+    </div>
+  </Row>
 )
 
 class Home extends Component {
   render () {
     return (
-        <div>
-          <Row className={styles.banner} type="flex" justify="center" style={{ backgroundImage: `url(${homeBanner})` }}>
-            <div className={globalStyles.content} style={{ marginTop: '200px', color: 'white' }}>
-              <div style={{ fontSize: '50px', fontWeight: '500' }}>Blockchain-based Credit Fraud Detection</div>
-              <div style={{ fontSize: '20px', lineHeight: '30px' }}>Secure joint computation platform for lending
-                companies for fraud
-                detection. <br />
-                Now available in the Philippines.
-              </div>
+      <div>
+        <Row className={styles.banner} type="flex" justify="center" style={{ backgroundImage: `url(${homeBanner})` }}>
+          <div className={globalStyles.content} style={{ marginTop: '200px', color: 'white' }}>
+            <div style={{ fontSize: '50px', fontWeight: '500' }}>Blockchain-based Credit Fraud Detection</div>
+            <div style={{ fontSize: '20px', lineHeight: '30px' }}>Secure joint computation platform for lending
+              companies for fraud
+              detection. <br />
+              Now available in the Philippines.
             </div>
-          </Row>
-          <Row className={styles.introduction} type="flex" align="middle" justify="center">
-            <div className={globalStyles.content} style={{ padding: '20px', textAlign: 'center' }}>
-              Defi Data Collaboration Protocol provides access to
-              high quality, secure and sharable data. The system allows easy processing, sharing and distribution of
-              data, while keeping the privacy of both enterprises and individuals intact.
-            </div>
-          </Row>
-          <Vision />
-          <Divider style={{ margin: 0 }} />
-          <Solutions />
-          <Technologies />
-          <Partners />
-        </div>
+          </div>
+        </Row>
+        <Row className={styles.introduction} type="flex" align="middle" justify="center">
+          <div className={globalStyles.content} style={{ padding: '20px', textAlign: 'center' }}>
+            Defi Data Collaboration Protocol provides access to
+            high quality, secure and sharable data. The system allows easy processing, sharing and distribution of
+            data, while keeping the privacy of both enterprises and individuals intact.
+          </div>
+        </Row>
+        <Vision />
+        <Divider style={{ margin: 0 }} />
+        <Solutions />
+        <Technologies />
+        <Partners />
+      </div>
     );
   }
 }
